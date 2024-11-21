0d2c5db7be8e377b0547c3ec8cebfee564f577f3
sreypheaphorn <sreyphea.phorn@institute.pse.ngo>
2024-11-21 14:09:13 +0700

    add

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());  
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; 
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
