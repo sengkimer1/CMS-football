965505abeba6e7fc683d6f79381c3b80f8994f7a
sengkimer1 <kimer.seng@institute.pse.ngo>
2024-11-25 13:35:05 +0700

    update role admin already

--- route/matchRoutes.js
+++ route/matchRoutes.js
@@ -14,24 +14,7 @@ const roleMiddleware = require('../Modelware/roleMiddleware');
 matchRouter.post('/match',authMiddleware,roleMiddleware('admin'), createMatch);
 matchRouter.get('/matchs',getAllMatch)
 matchRouter.get('/matchs/:id',getMatchID)
-
 matchRouter.put('/matchs/:id',authMiddleware,roleMiddleware('admin'),updateMatch)
 matchRouter.delete('/matchs/:id',authMiddleware,roleMiddleware('admin'),deleteMatch)
-
-// Route for getting all matches
-// matchRouter.get('/matches', getAllMatches);
-
-// Route for getting a match by ID
-// matchRouter.get('/match/:id', getMatchById);
-
-// Route for updating a match (general update)
-// matchRouter.put('/match/:id', updateMatch);
-
-// Route for canceling a match
-// matchRouter.put('/match/:id/cancel', cancelMatch);
-
-
-matchRouter.put('/matchs/:id',updateMatch)
-matchRouter.delete('/matchs/:id',deleteMatch)
 
 module.exports = matchRouter;
