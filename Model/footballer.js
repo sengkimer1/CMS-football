604319de218816124c9b663c9bf5bc3ec2b6d539
sengkimer1 <kimer.seng@institute.pse.ngo>
2024-11-28 08:26:25 +0700

    add team in footballer

--- Model/footballer.js
+++ Model/footballer.js
@@ -1,21 +1,17 @@
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
-
-// Define the footballer schema
 const footballerSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   position: { type: String, required: true },
-  age:{type :Number ,required:true},
-  team: { type: String, required: true },
+  age: { type: Number, required: true, min: 5 }, // Assuming players are at least 5 years old
+  team: { type: String, enum: ["U13", "U16", "U19", "Professional"], required: true },
   club: { type: String, required: true },
   country: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
-  height: { type: Number, required: true },
-  weight: { type: Number, required: true },
-  imageUrl: { type: String, required: false } 
-} , { 
-  timestamps: true,
+  height: { type: Number, required: true }, // in cm
+  weight: { type: Number, required: true }, // in kg
+  imageUrl: { type: String, required: true }
 });
-// Create and export the model
-module.exports = mongoose.model('Footballer', footballerSchema);
+
+module.exports = mongoose.model("Footballer", footballerSchema);
